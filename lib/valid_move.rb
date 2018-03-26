082b3d4c8186f66b45196e781e4334e383df9c8b
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-26 02:46:09 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,12 @@
 # code your #valid_move? method here
-def valid_move?(board, position)
-    puts 'true'
-    move(board, index, token)
-    display_board(board)
-   else
-    puts 'false'
-    turn(board)
+def valid_move?(board, index)
+
+  if index.between?(0,8) && !position_taken?(board, index)
+      puts 'this is a valid move'
+    return true
+  else
+   return false
   end
-  display_board(board)
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,position)
