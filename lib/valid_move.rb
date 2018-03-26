a096dd9bcb482e8ffb64e65671a4f941dc924599
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-26 02:42:23 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,18 @@
 # code your #valid_move? method here
-def valid_move?(board, position)
-end
+# def valid_move?(board, position)
+# end
 
+array = @board
+if array[arr_index][index]  == "X" ||
+    array[arr_index][index]  == "O"
+    invalid
+    puts "Where to :"
+    @move = gets.chomp.to_i
+    mapping(@move,@marker)
+    return true
+end
+return false
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,position)
   if board[position] == " "
